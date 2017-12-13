58b42ffc53726283b1bcb8d73f99b7adf06b1a81
Mẫn <vanman.lqd@gmail.com>
2017-12-13 21:52:07 +0700

    reafactor for something better

--- includes/connection.php
+++ includes/connection.php
@@ -9,6 +9,7 @@
         {
             die("Connection failed: " .mysqli_connect_error());
         } 
+        mysqli_set_charset($connection, 'utf8');
         mysqli_select_db($connection, "thicuoiki");
         return mysqli_query($connection, $query);
     }
