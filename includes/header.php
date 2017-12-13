242be1e286a200c0cf8929c30dad9c9dd8593350
thuan274 <doanminhthuan274@gmail.com>
2017-12-13 16:22:21 +0700

    done login

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,6 @@
+<?php 
+session_start()
+?>
 <!DOCTYPE html>
 <html>
    <head>
@@ -30,10 +33,11 @@
           </li>
 
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        <?php 
+          if ($_SESSION["user_id"]) {
+            echo '<span class="username">Hi, '.$_SESSION["username"]."</span>";
+          }
+        ?>
       </div>
     </nav>
 
--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+session_destroy();
+?>
+
 <!DOCTYPE html>
 <html>
    <head>
@@ -9,26 +14,63 @@
     <div class="container">
       <form 
         method="POST"
+        action="login.php"
         class="form-signin">
         <h2 class="form-signin-heading">Please sign in</h2>
 
         <div class="form-group row">
           <label for="inputUserName" class="col-sm-2 col-form-label">User name: </label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputUserName" required="" placeholder="Username">
+            <input type="text" class="form-control" name="username" id="inputUserName" required="" placeholder="Username">
           </div>
         </div>
 
         <div class="form-group row">
           <label for="inputPassword" class="col-sm-2 col-form-label">Password: </label>
           <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword" required="" placeholder="Password">
+            <input type="password" class="form-control" name="password" id="inputPassword" required="" placeholder="Password">
           </div>
         </div>
 
-        <button class="btn btn-primary " type="submit">Sign in</button>
+        <button class="btn btn-primary " name="btn-sign-in" type="submit">Sign in</button>
       </form>
 
     </div>
+
+
+    <?php 
+      // connect to database
+      require_once("includes/connection.php");
+
+      if (isset($_POST["btn-sign-in"])) {
+        // get info from form 
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+         //làm sạch các thẻ html các kí tự đặc biệt do người dùng tạo ra để tấn công database theo phương thức sql injection
+        $username = strip_tags($username); //loại bỏ các tag html
+        $username = addslashes($username);  //các dấu , / ..
+
+        $password = strip_tags($password);
+        $password = addslashes($password);
+
+        $sql = "SELECT * FROM user WHERE username='$username' and password='$password'";
+        $query = mysqli_query($connect, $sql);
+        $num_row = mysqli_num_rows($query);
+
+        if ($num_row == 0) {
+          echo "Invalid login credentials. Please try again!";
+        } else {
+
+          while ($data = mysqli_fetch_array($query)) {
+            $_SESSION["user_id"] = $data["Id"];
+            $_SESSION["username"] = $data["username"];
+          }
+
+          header('Location: index.php');  
+        }
+
+      }
+    ?>
   </body>
 </html>
