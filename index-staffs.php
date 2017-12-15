0612d67b4f8de9875afbe7726249fe6d41ed1178
thuan274 <doanminhthuan274@gmail.com>
2017-12-15 23:09:24 +0700

    auto input info staff in page update

--- index-staffs.php
+++ index-staffs.php
@@ -10,6 +10,7 @@
         <th>Address</th>
         <th>Email</th>
         <th>Position</th>
+        <th>Action</th>
     </tr>
     <?php
         require ('includes/connection.php');
@@ -24,9 +25,16 @@
             <td>".$row{"Name"}."</td>
             <td>".$row{"Address"}."</td>
             <td>".$row{"Email"}."</td>
-            <td>".$row{"Position"}."</td>
-            </tr>";
+            <td>".$row{"Position"}."</td>"
+        ?>
+            <td><button class="btn btn-info"  onClick="document.location.href='update-staff.php?Id=<?php echo $row{"Id"}; ?>'">Chỉnh sửa</button></td>
+            </tr>
+        <?php
         }
     ?>
 </table>
+
+<script>
+ 
+</script>
 <?php include("includes/footer.php") ?>
---
+++ update-staff.php
@@ -0,0 +1,65 @@
+<?php include ('includes/header.php') ?>
+
+<h3 class="text-center">Cập nhật thông tin</h3>
+
+<?php 
+    if (isset($_GET["Id"])) {
+      require ('includes/connection.php');
+      $query = "select * from staff where Id=".$_GET["Id"];
+      $data = ConnectDatabase($query);
+      while ($row = mysqli_fetch_assoc($data)) {
+        $positionId = $row{"PositionId"};
+      ?>
+        <form action="new-staff.php" method="POST" class="col-md-6 offset-md-3">
+
+          <div class="form-group">
+            <label for="name-staff">Tên nhân viên: </label>
+            <input 
+              type="text" name="name-staff" class="form-control" id="name-staff" required=""
+              value="<?php echo $row{"Name"}; ?>">
+          </div>
+
+          <div class="form-group">
+            <label for="email-staff">Email: </label>
+            <input 
+              type="email" name="email-staff" class="form-control" id="email-staff" required=""
+              value="<?php echo $row{"Email"}; ?>">
+          </div>
+
+          <div class="form-group">
+            <label for="address">Địa chỉ: </label>
+            <input 
+              type="text" name="address" class="form-control" id="address"
+              value="<?php echo $row{"Address"} ?>">
+          </div>
+
+          <div class="form-group">
+            <label for="Position">Chức vụ: </label>
+            <select name="position" class="form-control" size="1">
+              <?php 
+                $query2 = "select * from position";
+                $data2 = ConnectDatabase($query2);
+                while ($row2 = mysqli_fetch_assoc($data2)) {
+              ?>
+
+              <option 
+                value=<?php echo $row2['Id']; ?>
+                <?php if ($row2['Id'] == $positionId ) echo 'selected' ; ?> 
+                > 
+                <?php echo $row2['Name'] ?> </option>
+              <?php 
+                }
+              ?>
+            </select>
+          </div>
+
+          <button type="submit" class="btn btn-primary" name="btn-add-staff">Cập nhật</button>
+        </form>
+      <?php
+      }
+    }
+?>
+
+
+
+<?php include("includes/footer.php") ?>
